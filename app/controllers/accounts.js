a44a240667b662f0d3b11be03e92faf8664a9c51
Nihey Takizawa <nihey.takizawa@gmail.com>
2015-05-31 22:45:26 -0300

    Add possibility to add accounts to a namespace

---
+++ app/controllers/accounts.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+    addAccount: function() {
+      this.store.createRecord('account', {
+        name: this.get('account'),
+        namespace: this.model,
+      }).save().then(() => {
+        this.model.save();
+      });
+    },
+  },
+});
--- app/models/namespace.js
+++ app/models/namespace.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  accounts: DS.hasMany('account'),
+  accounts: DS.hasMany('account', {async: true}),
 });
