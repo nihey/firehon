ce6a395469670e6e840e03308e270b91d861c85e
Nihey Takizawa <nihey.takizawa@gmail.com>
2015-06-05 23:20:07 -0300

    Add date column on transactions page

--- app/models/transaction.js
+++ app/models/transaction.js
@@ -3,5 +3,8 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   description: DS.attr('string'),
   value: DS.attr('number'),
+  datetime: DS.attr('date', {defaultValue: function() {
+    return new Date();
+  }}),
   account: DS.belongsTo('account'),
 });
