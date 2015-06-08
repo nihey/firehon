021a12b8c0ebde6474a06d63c5ba410ee5eaa53e
Nihey Takizawa <nihey.takizawa@gmail.com>
2015-06-08 11:34:00 -0300

    Asyncly fetch transactions from an account

--- app/models/account.js
+++ app/models/account.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   namespace: DS.belongsTo('namespace'),
-  transactions: DS.hasMany('transaction'),
+  transactions: DS.hasMany('transaction', {async: true}),
 });
